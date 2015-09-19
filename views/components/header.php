3d327ac8b4799eccdd5209b3dffc6f786b4318fe
Zopper9000 <david.lu@hotmail.com>
2015-09-19 04:25:17 -0400

    added 420 blaze it map

--- views/components/header.php
+++ views/components/header.php
@@ -4,12 +4,12 @@
 
 <head>
     <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
-    <title>Whistlet</title>
+    <title>WhizBridge</title>
     <meta name="description" content="Say what you want. No strings attached.">
     <meta name="keywords" content="Whistlet, whistlet, social, microblogging, blogging, anonymous, temporary, network, networking, new, twitter, and, snapchat, hybrid">
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
     <link rel="icon" href="/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"> -->
     <link rel="stylesheet/less" href="/resources/styles/style.less">
     <script src="/resources/styles/less.js"></script>
     <script src="/resources/scripts/jquery-1.9.1.min.js"></script>
--- views/pages/home.php
+++ views/pages/home.php
@@ -12,14 +12,18 @@
 
 <body>
 
-
+<div id="map" style="height:400px; width:500px;"></div>
         <div id="content">
             <div class="wrapper">
 
                 <div class="panel left" style="position:fixed;">
+
                    Welcome
                    <?php  echo $cur_user->username; ?>
+
                 </div>
+
+
                 <br>
                 <form action = "logout">
                     <input type="submit" value="Logout" />
@@ -28,7 +32,13 @@
 
                 <div class="panel center" id="broadcast_panel">
                     <!--Add broadcasts here-->
+
+
                 </div>
             </div>
         </div>
+
+        <script src="geolocation.js"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBH5DWHzRW5NK60dvJt3ak-pdCgs3zsdec&signed_in=true&callback=initMap">
+        </script>
 </body>
