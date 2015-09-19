c3b034cce89dcc893e81bc5fb99dd5f8badbef95
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2015-09-19 16:40:43 -0400

    Updated css for login

--- views/pages/login.php
+++ views/pages/login.php
@@ -3,41 +3,40 @@
   <div id = "createForm" class = "dropdownBody" style = "display:none">
     <form action="postJob" method="post">
         <div class = "formBody">
-		<div class = "leftFields">
-          Job Name: <br>
-          Job Description: <br>
-          Price: <br>
-          Address: <br>
-          Email: <br>
-          Card Number: <br>
-          Expiration Date: <br>
-          CVV: <br>
-          Billing Address Line 1: <br>
-          Billing Address Line 2: <br>
-          City: <br>
-          State/Province: <br>
-          Country: <br>
-          Postal Code: <br>
-          Cardholder Name: <br>
-        </div>
-        <div class = "rightFields">
-           <input type="text" name="job_name"><br>
-           <input type="text" name="job_description"><br>
-           <input type="text" name = "job_price"><br>
-           <input type = "text" name = "address"><br>
-           <input type = "text" name = "email"><br>
-           <input data-braintree-name="number" value=""><br>
-           <input data-braintree-name="expiration_date" value=""><br>
-           <input data-braintree-name="cvv" value=""><br>
-           <input data-braintree-address1="address1" value=""><br>
-           <input data-braintree-address2="address2" value=""><br>
-           <input data-braintree-city="city" value=""><br>
-           <input data-braintree-pronvince="province" value=""><br>
-           <input data-braintree-country="country" value=""><br>
-           <input data-braintree-name="postal_code" value=""><br>
-           <input data-braintree-name="cardholder_name" value=""><br>
-        </div>
-		
+			<div class = "leftFields">
+			  Job Name: <br>
+			  Job Description: <br>
+			  Price: <br>
+			  Address: <br>
+			  Email: <br>
+			  Card Number: <br>
+			  Expiration Date: <br>
+			  CVV: <br>
+			  Billing Address Line 1: <br>
+			  Billing Address Line 2: <br>
+			  City: <br>
+			  State/Province: <br>
+			  Country: <br>
+			  Postal Code: <br>
+			  Cardholder Name: <br>
+			</div>
+			<div class = "rightFields">
+			   <input type="text" name="job_name"><br>
+			   <input type="text" name="job_description"><br>
+			   <input type="text" name = "job_price"><br>
+			   <input type = "text" name = "address"><br>
+			   <input type = "text" name = "email"><br>
+			   <input data-braintree-name="number" value=""><br>
+			   <input data-braintree-name="expiration_date" value=""><br>
+			   <input data-braintree-name="cvv" value=""><br>
+			   <input data-braintree-address1="address1" value=""><br>
+			   <input data-braintree-address2="address2" value=""><br>
+			   <input data-braintree-city="city" value=""><br>
+			   <input data-braintree-pronvince="province" value=""><br>
+			   <input data-braintree-country="country" value=""><br>
+			   <input data-braintree-name="postal_code" value=""><br>
+			   <input data-braintree-name="cardholder_name" value=""><br>
+			</div>
 		</div>
 		<br>
 		<div class = "submit">
@@ -49,24 +48,45 @@
   <div id = "loginButton" class = "dropdownButton"><h1>Login</h1></div>
   <div id = "loginForm" class = "dropdownBody" style = "display:none">
     <form action="attemptLogin" method="post">
-        <table align = "center">
-          <tr> <td style="float:right"> Username: </td> <td> <input type="text" name="username"> </td> </tr>
-          <tr> <td style="float:right"> Password: </td> <td> <input type="password" name="password"> </td> </tr>
-        </table>
-        <input type="submit" value="Login">
+		<div class = "formBody">
+			<div class = "leftFields">
+			  Username: <br>
+			  Password: <br>
+			</div>
+			<div class = "rightFields">
+			   <input type="text" name="username"><br>
+			   <input type="password" name="password"><br>
+			</div>
+		</div>
+		<br>
+		<div class = "submit">
+			<input type="submit" value="Login">
+		</div>
     </form>
   </div>
   <div id = "registerButton" class = "dropdownButton"><h1>Register</h1></div>
   <div id = "registerForm" class = "dropdownBody" style = "display:none">
     <form action="attemptRegistration" method="post">
-        <table align = "center">
-          <tr> <td style="float:right">Username:</td> <td> <input type="text" name="username"> </td></tr>
-          <tr> <td style="float:right">Password: </td> <td> <input type = "password" name = "password"> </td> </tr>
-          <tr> <td style="float:right">Confirm Password: </td> <td> <input type = "password" name = "password2"></td> </tr>
-          <tr> <td style="float:right">Full Name: </td><td> <input type="text" name="full_name"></td> </tr>
-          <tr> <td style="float:right">Email: </td><td> <input type="text" name="email"></td> </tr>
-        </table>
-        <input type="submit" value="Register">
+		<div class = "formBody">
+			<div class = "leftFields">
+			  Username: <br>
+			  Password: <br>
+			  Confirm Password: <br>
+			  Full Name: <br>
+			  Email: <br>
+			</div>
+			<div class = "rightFields">
+			   <input type="text" name="username"><br>
+			   <input type = "password" name = "password"><br>
+			   <input type = "password" name = "password2"><br>
+			   <input type="text" name="full_name"><br>
+			   <input type="text" name="email"><br>
+			</div>
+		</div>
+		<br>
+		<div class = "submit">
+			<input type="submit" value="Register">
+		</div>
     </form>
   </div>
   <br>
