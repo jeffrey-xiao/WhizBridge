d6d18fcc0730ab301099f195a32432080e3c3720
Zopper9000 <david.lu@hotmail.com>
2015-09-19 06:28:18 -0400

    renamed maps.js and added loadmarkers function

--- views/pages/maps.js
+++ views/pages/maps.js
@@ -1,9 +1,10 @@
+var map;
 function initMap() {
     var mapOpt = {
         center: {lat: 43.47229, lng: -80.54486},
         zoom: 16
     };
-    var map = new google.maps.Map(document.getElementById('map'), mapOpt);
+    map = new google.maps.Map(document.getElementById('map'), mapOpt);
     var infoWindow = new google.maps.InfoWindow({map: map});
     var pos = mapOpt.center;
     // Try HTML5 geolocation.
@@ -39,4 +40,24 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setContent(browserHasGeolocation ?
         'Error: The Geolocation service failed.' :
         'Error: Your browser doesn\'t support geolocation.');
+}
+
+function loadMarkers(locations) {
+    var marker, i;
+
+    for(i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map
+        })
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infowindow.setContent(locations[i][0]);
+                infowindow.open(map, marker);
+            }
+
+        })(marker, i));
+    }
+
+
 }
