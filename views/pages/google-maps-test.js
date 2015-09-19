ff0bf49a00110d57fa90169184e84f2fa471d883
Zopper9000 <david.lu@hotmail.com>
2015-09-19 06:03:54 -0400

    fixed geolocation
    fucking magic......

--- views/pages/google-maps-test.js
+++ views/pages/google-maps-test.js
@@ -1,27 +1,33 @@
-/**
- * Created by David on 2015-09-19.
- */
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 6
-    });
+    var mapOpt = {
+        center: {lat: 43.47229, lng: -80.54486},
+        zoom: 16
+    };
+    var map = new google.maps.Map(document.getElementById('map'), mapOpt);
     var infoWindow = new google.maps.InfoWindow({map: map});
-
+    var pos = mapOpt.center;
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
+            pos = {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
             };
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
+            //infoWindow.setPosition(pos);
+            //infoWindow.setContent('Location found.');
             map.setCenter(pos);
+
+            var marker = new google.maps.Marker({
+                position: pos,
+                map: map,
+                title: 'My Location'
+            });
+
         }, function() {
             handleLocationError(true, infoWindow, map.getCenter());
         });
+        map.setZoom(16);
     } else {
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
--- views/pages/home.php
+++ views/pages/home.php
@@ -8,19 +8,29 @@
 
 </script>
 
+<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+<meta charset="utf-8">
+<style>
+    html, body {
+        height: 60%;
+        margin: 0;
+        padding: 0;
+    }
+    #map {
+        height: 60%;
+    }
+</style>
+
 </head>
 
 <body>
 
-<div id="map" style="height:400px; width:500px;"></div>
         <div id="content">
-            <div class="wrapper">
 
+            <div class="wrapper"><div id="map" style="width:500px; height:500px"></div>
                 <div class="panel left" style="position:fixed;">
-
                    Welcome
                    <?php  echo $cur_user->username; ?>
-
                 </div>
 
 
@@ -32,13 +42,12 @@
 
                 <div class="panel center" id="broadcast_panel">
                     <!--Add broadcasts here-->
-
-
                 </div>
             </div>
         </div>
 
-        <script src="geolocation.js"></script>
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBH5DWHzRW5NK60dvJt3ak-pdCgs3zsdec&signed_in=true&callback=initMap">
+        <script src="google-maps-test.js"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBH5DWHzRW5NK60dvJt3ak-pdCgs3zsdec&signed_in=true&callback=initMap"
+                async defer>
         </script>
 </body>
