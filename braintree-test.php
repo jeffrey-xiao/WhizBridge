7a1053a58b098e03de929dd48b80d31694dbda72
Zopper9000 <david.lu@hotmail.com>
2015-09-19 02:02:11 -0400

    braintree payment api testing

---
+++ braintree-test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: David
+ * Date: 2015-09-19
+ * Time: 1:54 AM
+ */
+
+require "braintree-php/lib/Braintree.php";
+Braintree_Configuration::environment('sandbox');
+Braintree_Configuration::merchantId('f8hz5cfs97665c2r');
+Braintree_Configuration::publicKey('289njc8rrdf47cwk');
+Braintree_Configuration::privateKey('e23e1e9b16adfccd3208096b42bba5a9');
+
+echo($clientToken = Braintree_ClientToken::generate());
