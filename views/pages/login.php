2cba1b85632776f3a614bc0305fc72b7d3170815
Zopper9000 <david.lu@hotmail.com>
2015-09-19 13:26:13 -0400

    made shit pretty

--- views/pages/login.php
+++ views/pages/login.php
@@ -1,28 +1,27 @@
+<h1>Login</h1>
 <form action="attemptLogin" method="post">
 Username: <input type="text" name="username"><br>
 Password: <input type="password" name="password"><br>
-<input type="submit">
+<input type="submit" value="Login">
 </form>
-</br>
-</br>
+<br>
+<h1>Register</h1>
 <form action="attemptRegistration" method="post">
 Username: <input type="text" name="username"><br>
 Password: <input type = "password" name = "password"><br>
 Confirm Password: <input type = "password" name = "password2"><br>
 Full Name: <input type="text" name="full_name"><br>
 Email: <input type="text" name="email"><br>
-<input type="submit">
+<input type="submit" value="Register">
 </form>
-
-
+<br>
+<h1>Create Job</h1>
 <form action="postJob" method="post">
 Job Name: <input type="text" name="job_name"><br>
 Job Description: <input type="text" name="job_description"><br>
 Price: <input type="text" name = "job_price"><br>
 Address: <input type = "text" name = "address" value = "1 Abbey Rd, Welland, ON L3C 7L1"><br>
 Email: <input type = "text" name = "email"> <br>
-
-<input type="submit">
     Card Number: <input data-braintree-name="number" value=""> <br>
     Expiration Date: <input data-braintree-name="expiration_date" value=""> <br>
     CVV: <input data-braintree-name="cvv" value=""> <br>
