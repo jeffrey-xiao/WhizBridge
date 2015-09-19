7449f06589ede7e2ef9909a5454b12432e8881f8
Zopper9000 <david.lu@hotmail.com>
2015-09-19 11:29:29 -0400

    added processpayment
    using braintree

--- controller.php
+++ controller.php
@@ -1,6 +1,7 @@
 <?php
 require("model.php");
 require "twilio-php/services/Twilio.php";
+require "braintree-php/lib/Braintree.php";
 
 class Controller
 {
@@ -324,6 +325,43 @@ class Controller
             "To" => "647-262-0171",
             "Body" => "Welcome to Twilio!",
         ));
+    }
+
+    private function processPayment(){
+        Braintree_Configuration::environment('sandbox');
+        Braintree_Configuration::merchantId('f8hz5cfs97665c2r');
+        Braintree_Configuration::publicKey('289njc8rrdf47cwk');
+        Braintree_Configuration::privateKey('e23e1e9b16adfccd3208096b42bba5a9');
+
+        //echo($clientToken = Braintree_ClientToken::generate());
+
+        //Process payment
+        $nonce = $_POST["payment_method_nonce"];
+        /* Use payment method nonce here */
+
+        //Amount of payment
+        $amount = $_POST["payment_amount"];
+
+        $result = Braintree.Transaction::sale([
+                'amount' => $amount,
+                'paymentMethodNonce' => $nonce
+            ]);
+        $name = "Valid payment";
+        switch ($nonce) {
+            case "fake-valid-visa-nonce":
+                $name = "Visa";
+                break;
+            case "fake-valid-mastercard-nonce":
+                $name = "Mastercard";
+                break;
+            case "fake-valid-debit-nonce":
+                $name = "Debit";
+                break;
+            default:
+                break;
+        }
+
+        //echo($name . " for $" . $amount . "\n");
     }
 }
 
