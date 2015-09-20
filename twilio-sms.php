bc98b751a9409ad095b470b7525d5e1eddf35559
Zopper9000 <david.lu@hotmail.com>
2015-09-19 23:19:58 -0400

    added sms

--- twilio-sms.php
+++ twilio-sms.php
@@ -1,3 +1,4 @@
+
 <?php
 /**
  * Created by PhpStorm.
@@ -5,40 +6,40 @@
  * Date: 2015-09-19
  * Time: 9:30 PM
  */
-require ('controller.php');
-require ('model.php');
-$AccountSid = "ACb821cce27750bb57b5726316b4e4a7e1";
+//echo 'Derp';
+$content = "TestTest123";
+//require_once ("twilio-php/services/Twilio.php");
+// set your AccountSid and AuthToken from www.twilio.com/user/account
+/*$AccountSid = "ACb821cce27750bb57b5726316b4e4a7e1";
 $AuthToken = "32da8a446b9da43c0a3c7b426bf20c09";
 
 $client = new Services_Twilio($AccountSid, $AuthToken);
 
 foreach($client->account->messages as $message) {
-    //echo $message->body;
-    $msg = strtolower($message->body);
-    $content = "TestTest123";
-    if($msg == 'cancel' && getJobIdForPhonenumber($message->from) != false) {
-        cancelWhizJob(getJobIdForPhonenumber($message->from));
-        $content = "Job cancelled.";
-    }
-    else if($msg == 'complete' && getJobIdForPhonenumber($message->from) != false) {
-        completeWhizJob(getJobIdForPhonenumber($message->from));
-        $content = "Job completed.";
-    }
-    else if($msg == 'tech') {
-        $content = "Please enter your: Name, Problem description, Address, and Price.";
-    }
-    else {
-        if(!getJobIdForPhonenumber($message->from)) {
-            sendSMS($message->from, "Unknown WhizBridge text, please try again.");
-        }
-        else {
-            //Process job
-        }
-    }
+    echo $message->body;
 
-    header("content-type: text/xml");
-    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+
+
+}*/
+$msg = $_REQUEST['Body'];
+$msg = strtolower($msg);
+if ($msg == 'escape') {
+    //cancelWhizJob(getJobIdForPhonenumber($message->from));
+    $content = "Job cancelled.";
+} else if ($msg == 'complete') {
+    //completeWhizJob(getJobIdForPhonenumber($message->from));
+    $content = "Job completed.";
+} else if ($msg == 'tech') {
+    $content = "Please enter your: Name, Problem description, Address, and Price.";
 }
+    else if(substr($msg, 0, 5) == 'david')
+    {
+        $content = "Job submitted, payment processing.";
+    }
+else {
+    $content = "Unknown WhizBridge text, please try again.";
+}
+
 
 ?>
 
